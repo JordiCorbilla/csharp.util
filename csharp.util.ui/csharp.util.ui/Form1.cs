c0845dbe76df38348bdea558da84371e2f65427c
JordiCorbilla <jordi.coll.corbilla@gmail.com>
2018-08-12 15:47:29 +0100

    create utility suite

---
+++ csharp.util.ui/csharp.util.ui/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace csharp.util.ui
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string newline;
+            foreach(var line in textBox1.Lines)
+            {
+                newline = line;
+                newline = newline.Replace(@"""", @"\""");
+                newline = "\"" + newline + "\"+ ";
+                textBox2.AppendText(newline + Environment.NewLine);
+            }
+        }
+    }
+}
